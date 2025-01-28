124630c6c545097ed44fec6920583726f6adee2e
aspeak03524 <aspeak03524@gmail.com>
2025-01-28 14:24:40 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "eu-west-1"
+  region = "eu-south-2"
 }
 
 resource "aws_vpc" "eks_vpc" {
@@ -13,13 +13,13 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "eu-west-1a"
+  availability_zone       = "eu-south-2a"
   map_public_ip_on_launch = true
 }
 resource "aws_subnet" "public_subnet_2" {
   vpc_id                  = aws_vpc.eks_vpc.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "eu-west-1b"
+  availability_zone       = "eu-south-2b"
   map_public_ip_on_launch = true
 }
 
@@ -42,7 +42,7 @@ resource "aws_route_table_association" "public_subnet_2_assoc" {
 }
 
 resource "aws_iam_role" "eks_role" {
-  name = "andrew-role14"
+  name = "andrew-role114"
   assume_role_policy = jsonencode({
     Statement = [{
       Action = "sts:AssumeRole"
@@ -81,7 +81,7 @@ resource "aws_eks_cluster" "expense_tracker" {
 }
 
 resource "aws_iam_role" "eks_nodegroup_role" {
-  name = "andrew-14"
+  name = "andrew-114"
   assume_role_policy = jsonencode({
     Statement = [
       {
