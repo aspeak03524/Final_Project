2a53c3f17aaf1fc756eb99c6da908294d3b9945b
aspeak03524 <aspeak03524@gmail.com>
2025-01-29 16:37:10 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,7 +42,7 @@ resource "aws_route_table_association" "public_subnet_2_assoc" {
 }
 
 resource "aws_iam_role" "eks_role" {
-  name = "andrew-role5"
+  name = "andrew-role6"
   assume_role_policy = jsonencode({
     Statement = [{
       Action = "sts:AssumeRole"
@@ -65,7 +65,7 @@ resource "aws_iam_role_policy_attachment" "cluster_attach_2" {
 }
 
 resource "aws_eks_cluster" "expense_tracker" {
-  name     = "andrew-cluster5"
+  name     = "andrew-cluster6"
   role_arn = aws_iam_role.eks_role.arn
   
   vpc_config {
@@ -81,7 +81,7 @@ resource "aws_eks_cluster" "expense_tracker" {
 }
 
 resource "aws_iam_role" "eks_nodegroup_role" {
-  name = "andrew-5"
+  name = "andrew-6"
   assume_role_policy = jsonencode({
     Statement = [
       {
